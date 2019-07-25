deeb537771e4e751a1047861608a2d5c2cb0848c
Mykhaylo Herasika <mykhaylo.herasika@globaltechmakers.com>
2019-07-25 09:45:14 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var myIndex = /** @class */ (function () {
+    function myIndex() {
+    }
+    myIndex.prototype.log = function () {
+        console.log('hello world');
+    };
+    return myIndex;
+}());
+exports.myIndex = myIndex;
